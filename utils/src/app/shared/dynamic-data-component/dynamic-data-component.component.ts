baa79816d9a4f1bd8191c80f67b8e2c70439589a
nikhil <nikhilchawla9013@gmail.com>
2025-08-20 15:44:05 +0530

    add some styling support for label

--- utils/src/app/shared/dynamic-data-component/dynamic-data-component.component.ts
+++ utils/src/app/shared/dynamic-data-component/dynamic-data-component.component.ts
@@ -8,27 +8,55 @@ import { DynamicFieldType } from '../enums';
   styleUrl: './dynamic-data-component.component.css',
 })
 export class DynamicDataComponentComponent implements OnInit {
+  parentClass = 'd-flex header';
   fieldConfig: DynamicField[] = [
     {
       type: DynamicFieldType.LABEL,
       label: 'First Name',
+      parentClass: 'col',
+         fieldClass: 'm-2 font-bold',
+              valueClass: 'font-low'
     },
     {
       type: DynamicFieldType.LABEL,
       label: 'Second Name',
+        parentClass: 'col',
+           fieldClass: 'm-2 font-bold',
+              valueClass: 'font-low'
     },
     {
       type: DynamicFieldType.LABEL,
       label: 'Last Name',
+        fieldClass: 'm-2 font-bold',
+              valueClass: 'font-low',
+        parentClass: 'col'
     },
     {
       type: DynamicFieldType.GROUP,
       title: 'Address',
-      parentClass:'d-flex gap-2',
+      groupClass: 'd-flex gap-2 address-col',
+      parentClass: 'p-5',
+         fieldClass: 'm-2 font-bold',
+              valueClass: 'font-low',
       fields: [
         {
-          type: DynamicFieldType.LABEL,
-          label: 'First Name',
+          type: DynamicFieldType.GROUP,
+          fields: [
+            {
+              type: DynamicFieldType.LABEL,
+              label: 'First Name I',
+              fieldClass: 'font-bold',
+              valueClass: 'font-low'
+            },
+            {
+              type: DynamicFieldType.LABEL,
+              label: 'Second Name I',
+            },
+            {
+              type: DynamicFieldType.LABEL,
+              label: 'Last Name I',
+            },
+          ],
         },
         {
           type: DynamicFieldType.LABEL,
@@ -44,6 +72,6 @@ export class DynamicDataComponentComponent implements OnInit {
   DynamicFieldType = DynamicFieldType;
 
   ngOnInit(): void {
-      console.log(this.fieldConfig);
+    console.log(this.fieldConfig);
   }
 }
--- utils/src/app/shared/interfaces.ts
+++ utils/src/app/shared/interfaces.ts
@@ -2,13 +2,13 @@ import { DynamicFieldType } from "./enums";
 
 export interface DynamicField {
     readonly type: DynamicFieldType.BUTTON | DynamicFieldType.COMPONENT | DynamicFieldType.GROUP | DynamicFieldType.LABEL,
-    readonly value?:string,
-    readonly label?:string,
-    readonly class?:string,
-    readonly title?:string,
-    onClick?:()=>void,
-    fields?:DynamicField[],
-    readonly fieldClass?:string,
-    readonly valueClass?:string,
-    readonly parentClass?:string,
+    readonly value?:string, //field value
+    readonly label?:string, //field label
+    readonly title?:string, // group title
+    onClick?:()=>void, //button onclick
+    fields?:DynamicField[], // group fields
+    readonly fieldClass?:string, //field class
+    readonly valueClass?:string, //field value class
+    readonly parentClass?:string, //field parent div clas
+    readonly groupClass?:string //group parent div class
 }
